88c91e6edb875a01cf0a2f50d334f1380bf2d4e2
duynguyen <duynguyen@local.drumee>
2025-10-28 21:30:19 -0700

    updated

---
+++ schemas/procedures/get_countries.sql
@@ -0,0 +1,28 @@
+-- File: onboarding-server/sql/procedures/get_countries.sql
+DROP PROCEDURE IF EXISTS `get_countries`;
+
+DELIMITER $$
+
+CREATE PROCEDURE `get_countries`(
+    IN _locale_code VARCHAR(10) COLLATE utf8mb4_unicode_ci 
+)
+BEGIN
+    IF _locale_code IS NULL OR _locale_code = '' THEN
+        SELECT 
+            country_code, 
+            locale_code, 
+            locale_name 
+        FROM countries
+        ORDER BY locale_name ASC; 
+    ELSE
+        SELECT 
+            country_code, 
+            locale_code, 
+            locale_name 
+        FROM countries
+        WHERE locale_code = _locale_code
+        ORDER BY locale_name ASC;
+    END IF;
+END$$
+
+DELIMITER ;
